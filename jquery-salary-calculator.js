d1c8e14447dd2a35ab76f423ce295c61acb9f26b
Iris Noh <irisnoh22@gmail.com>
2019-10-04 16:34:13 -0500

    console log all inputs

--- jquery-salary-calculator.js
+++ jquery-salary-calculator.js
@@ -11,5 +11,17 @@ function addNew () {
     const addFirstName = $('submitFirstName').val();
     console.log ($('#submitFirstName').val());
 
-    //const addLastName = $('submitLastName').val();
+    const addLastName = $('submitLastName').val();
+    console.log($('#submitLastName').val());
+
+    const addId = $('submitId').val();
+    console.log($('#submitId').val());
+
+    const addTitle = $('submitTitle').val();
+    console.log($('#submitTitle').val());
+
+    const addAnnualSalary = $('submitAnnualSalary').val();
+    console.log($('#submitAnnualSalary').val());
+
+
 }
