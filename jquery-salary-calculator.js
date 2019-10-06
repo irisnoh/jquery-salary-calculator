7e1bfef75ef1dba56fbc57a7363f15aadf2a8e3a
Iris Noh <irisnoh22@gmail.com>
2019-10-06 12:08:50 -0500

    push new input into table with an array

--- jquery-salary-calculator.js
+++ jquery-salary-calculator.js
@@ -16,12 +16,39 @@ function onReady() {
     console.log('in jquery!');
     $('#submitButton').on('click', addNew)
     $('tbody').on('click', '.deleteButton', deleteRow)
-    $('#totalMonthly').text(totalMonthlySalary)
+    //('#totalMonthly').text(totalMonthlySalary)
+}
+//need to make this an array?
+function addNew() {
+console.log ('in my addNew');
+    let newEmployeeObject = {
+        firstName: $('#submitFirstName').val(),
+        lastName: $('#submitLastName').val(),
+        id: $('#submitId').val(),
+        title: $('#submitTitle').val(),
+        annualSalary: $('#submitAnnualSalary').val()
+
+    }
+employeeObject.push(newEmployeeObject);
+    $(`tbody`).append(`
+        <tr>
+                <td> </td>
+                <td> </td>
+                <td> </td>
+                <td> </td>
+                <td> </td>
+                <td>
+                    <button class="deleteButton"> Delete </button>
+                </td>
+            </tr>`);
+
+} 
+
+function deleteRow() {
+    $(this).closest('tr').remove()
+    console.log('this was deleted');
 }
 
-function addingEmployees () {
-
-}
 /*function totalMonthlySalary() {
     let totals = [0];
     console.log('is this totaling?');
@@ -40,44 +67,4 @@ console.log (tb);
         $(this).text(Number.totals[i]);
     });
 
-}; */
-
-
-
-//need to make this an array?
-function addNew() {
-    console.log('in addNew');
-    //const addFirstName = $('#submitFirstName').val();
-    console.log($('#submitFirstName').val());
-
-    //const addLastName = $('#submitLastName').val();
-
-    console.log($('#submitLastName').val());
-
-   //const addId = $('#submitId').val();
-    console.log($('#submitId').val());
-
-    //const addTitle = $('#submitTitle').val();
-    console.log($('#submitTitle').val());
-
-    //const addAnnualSalary = $('#submitAnnualSalary').val();
-    console.log($('#submitAnnualSalary').val());
-
-    $(`tbody`).append(`
-        <tr>
-                <td>${addFirstName}</td>
-                <td>${addLastName}</td>
-                <td>${addId}</td>
-                <td>${addTitle}</td>
-                <td>${addAnnualSalary}</td>
-                <td>
-                    <button class="deleteButton"> Delete </button>
-                </td>
-            </tr>`);
-
-}
-
-function deleteRow() {
-    $(this).closest('tr').remove()
-    console.log('this was deleted');
-}
+};*/
