02570cf5ec15d8b9e7f2877e4c26dde27958d9ce
Iris Noh <irisnoh22@gmail.com>
2019-10-07 22:59:18 -0500

    added updates with feedback

--- README.md
+++ README.md
@@ -15,21 +15,34 @@ Additional README details can be found [here](https://github.com/PrimeAcademy/gi
     [x]- create another header for "Employees" in html
         [x]- create a submit button in html
            [x] - create a function that inputs new information into the table
+           [x] - add event listener for submit button
+            [x] - write handle submit
+            [x] - get inputs from input boxes (console log
+            [x] - create new employee object
+            [x] - push new employee object to array
+            [x] - append employee rows to DOM   
+            [x] - clear input values
+            [x] - calculate monthly total
+
 [x]- create a table with "first name", "last name", "ID", "title", and "annual salary" in html
     [x]- hard-code information into table in html
     [x]- create a "delete" button in html
-        - create a function that deletes the whole row when clicked
-[x]- create a line for Total Monthly: "salary" on the buttom
+        [x] - add event listener for delete buttons
+            [x] - create a function for handling delete
+            [x] - remove row from table
+            [x] - remove employee from array
+            [x] - calculate monthly total
+[x]- create a line for Total Monthly: "salary" on the bottom
     - create a function that totals all the salary in the chart
-- add some css style at the end!
+[x]- add some css style at the end!
 
-- annual salary should be a $
+[x]- annual salary should be a $
 
 [x]The application should have an input form that collects employee first name, last name, ID number, job title, annual salary.
 
-A 'Submit' button should collect the form information, store the information to calculate monthly costs, append information to the DOM and clear the input fields. Using the stored information, calculate monthly costs and append this to the to DOM. If the total monthly cost exceeds $20,000, add a red background color to the total monthly cost.
+[x]A 'Submit' button should collect the form information, store the information to calculate monthly costs, append information to the DOM and clear the input fields. Using the stored information, calculate monthly costs and append this to the to DOM. If the total monthly cost exceeds $20,000, add a red background color to the total monthly cost.
 
-Create a delete button that removes an employee from the DOM. For Base mode, it does not need to remove that Employee's salary from the reported total.
+[x]Create a delete button that removes an employee from the DOM. For Base mode, it does not need to remove that Employee's salary from the reported total.
 
 Files Provided
 No files have been provided. Fork and clone this repository or create a new GitHub repository to get started. Make sure to commit regularily!
--- jquery-salary-calculator.js
+++ jquery-salary-calculator.js
@@ -2,20 +2,13 @@ console.log('in js');
 
 $(document).ready(onReady);
 
-let employeeObject = [
-    {
-        firstName: '',
-        lastName: '',
-        id: '',
-        title: '',
-        annualSalary: '',
-    }
-];
+let employeeObject = []
+    ;
 
 function onReady() {
     console.log('in jquery!');
     $('#submitButton').on('click', addNew)
-    $('tbody').on('click', '.deleteButton', deleteRow)
+    $('tbody').on('click', '.deleteButton', deleteRow) // needs to be on tbody because on delete didn't happen yet
     $('#totalMonthly').text(totalMonthlySalary)
 }
 // add inputs to append to table
@@ -27,26 +20,27 @@ function addNew() {
         id: $('#submitId').val(),
         title: $('#submitTitle').val(),
         annualSalary: $('#submitAnnualSalary').val()
-
     }
+    // could have also done ${newEmployeeObject.firstName} instead of + newEmployeeObject.firstName +
     employeeObject.push(newEmployeeObject);
-    $(`tbody`).append(`
+    $(`#employeeRows`).append(`
         <tr>
-                <td>` + newEmployeeObject.firstName + `</td>
+                <td>` + newEmployeeObject.firstName + `</td>  
                 <td>` + newEmployeeObject.lastName + `</td>
                 <td>` + newEmployeeObject.id + `</td>
                 <td>` + newEmployeeObject.title + `</td>
-                <td>` + dollarSign.format(newEmployeeObject.annualSalary)+ `</td>
+                <td>` + dollarSign.format(newEmployeeObject.annualSalary) + `</td>
                 <td>
         <button class="deleteButton"> Delete </button> </td>
             </tr>`);
-    $('.inputs').val('');
+    $('.inputs').val(''); // this will empty the inputs
     totalMonthlySalary();
 
 }
 // calculates monthly salary and to display total
-function totalMonthlySalary() { 
+function totalMonthlySalary() {
     let totalYearlySalary = 0;
+    // could do employeeObject.forEach(function(employeeObject)) instead of a for loop as well
     for (let i = 0; i < employeeObject.length; i++) {
         console.log('is this totaling?');
         totalYearlySalary += Number(employeeObject[i].annualSalary);
@@ -59,28 +53,64 @@ function totalMonthlySalary() {
     $('#totalMonthly').text(dollarSign.format(monthlySalary));
     checkMonthlySalary();
 
-function checkMonthlySalary() {
-    console.log('checking Red');
-    if (monthlySalary < 20000) {
-        $('#totalMonthly').removeClass('red');
-        return true;
-    } else {
-        $('#totalMonthly').addClass('red');
-        return false;
+    function checkMonthlySalary() {
+        console.log('checking Red');
+        if (monthlySalary < 20000) {
+            $('#totalMonthly').removeClass('red');
+            return true;
+        } else {
+            $('#totalMonthly').addClass('red');
+            return false;
+        }
     }
-}
 }
 
 // deletes whole row
-function deleteRow() { 
-    $(this).closest('tr').remove()
-    console.log('this was deleted');
+function deleteRow() {
+        $(this).closest('tr').remove()
+        console.log('this was deleted');
+        /*
+    let employeeId = $(this).parent().prev().prev().prev().text();
+     console.log(employeeId);
+     let updatedEmployeeArray = [];
+     employeeObject.forEach(function (employee) {
+         if (employee.employeeId !== employeeId) {
+             updatedEmployeeArray.push(employee);
+         }
+     });
+     //$(this).closest('tr').remove()
+     console.log('this was deleted');
+     employeeObject = updatedEmployeeArray;
+     // display all employees
+     //update total
+    // renderEmployeeList();
+     totalMonthlySalary();*/
+
 }
 
-const dollarSign = new Intl.NumberFormat ('en-US', {
+/*
+function deleteEmployee() {
+    console.log('delete button has been clicked');
+    let employeeId = $(this).parent().prev().prev().prev().text();
+    console.log(employeeId);
+    // remove employee
+    let updatedEmployeeArray = [];
+    employeeObject.forEach(function (employee) {
+        if (newEmployeeObject.id !== employeeId) {
+            updatedEmployeeArray.push(employee);
+        }
+    });
+    employeeObject = updatedEmployeeArray;
+    // display all employees
+    addNew();
+    // update total
+    updateMonthlyTotal();
+}*/
+
+
+
+const dollarSign = new Intl.NumberFormat('en-US', {
     style: 'currency',
-    currency: 'USD', 
+    currency: 'USD',
     minimumFractionDigits: 2
 })
-
-
